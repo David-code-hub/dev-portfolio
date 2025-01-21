e8a992b3d379e02d0de6c6a1a51283d4f639896f
David <david@rlabs.org>
2025-01-21 21:58:47 +0200

    chore: add nuxt color mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@nuxtjs/tailwindcss']
+  modules: [
+    '@nuxt/icon',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/fonts',
+    '@nuxtjs/color-mode'
+  ]
 })
